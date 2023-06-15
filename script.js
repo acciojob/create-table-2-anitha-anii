d804c6bd4d681fd7ecfe70eb74604229358c8af8
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-15 08:11:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
     //Write your code here
+	
+let promptValue =prompt('Input number of rows'); 
+
+let table = document.getElementById('myTable');
+for (let i = 0; i < promptValue; i++) {
+   let Row = table.insertRow(i);
+   
+for (let j = 0;  j< 3; j++) {
+    let data =Row.insertCell(j);
+    data.innerText = `Row-${i} ,Column-${j}`;
   
 }
+}
+}
+
